f508dfee9aa74c0ec1f39a05e9ba87660aa182ec
HaydenMcKenzie <Haydendanielmckenzie@gmail.com>
2023-11-22 20:35:06 +1100

    Setup Scala.js

---
+++ scala-js-work/src/main/scala/tutorial/MainApp.scala
@@ -0,0 +1,17 @@
+package tutorial
+
+import org.scalajs.dom
+import org.scalajs.dom.document
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    appendPar(document.body, "Hello World")
+  }
+
+  def appendPar(targetNode: dom.Node, text: String): Unit = {
+    val parNode = document.createElement("p")
+    parNode.textContent = text
+    targetNode.appendChild(parNode)
+  }
+}
